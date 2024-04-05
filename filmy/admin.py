32a3020f82e462469a09ec67ee6ad05d0a6043ce
sabi <sabina.javurkova@student.gyarab.cz>
2024-04-05 11:24:20 +0200

    third commit

--- filmy/admin.py
+++ filmy/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from .models import film
 
+# abych nezapomnela: superuser 'admin' 'unipasswd'
+
 class FilmAdmin(admin.ModelAdmin):
-    list_display = ('title', 'year')
+    # list_display = ('title', 'year')
+    pass
 
 admin.site.register(film, FilmAdmin)
--- filmy/models.py
+++ filmy/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 class film(models.Model):
     title = models.CharField(max_length=30)
-    year = models.PositiveSmallIntegerField()
+    year = models.PositiveSmallIntegerField(blank=True, null=True) # nepovinne, proto blank a null
+    footage = models.PositiveSmallIntegerField(blank=True, null=True, help_text='in minutes') # delka filmu
     
+    def __str__(self):
+        return self.title
 
