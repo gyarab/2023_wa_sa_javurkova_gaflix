8e20aa577bf9531fe6c4562c11325765c044ec20
sabi <sabina.javurkova@student.gyarab.cz>
2024-05-24 11:20:02 +0200

    Vazby mezi models a uprava admin site

--- filmy/admin.py
+++ filmy/admin.py
@@ -2,7 +2,17 @@ from django.contrib import admin
 from .models import Movie, Actor, Director, Genre
 
 class MovieAdmin(admin.ModelAdmin):
-    pass
+    # genres je ManyToManyField, museli jsme definovat vlastni diplay funkci v models.py
+    list_display = ['id', 'name', 'year', 'genres_display', 'footage', 'director']
+    # ze zobrazovanych atributu udelam links, muzu na ne klikat
+    list_display_links = ['id', 'name']
+    # search bar
+    # '=field' -> hleda se exact match
+    search_fields = ['=id', 'name', 'director__name']
+    # filtrovani
+    list_filter = ['year', 'genres']
+    # to co je v 'list_display_links' se neda dat sem
+    list_editable = ['year', 'footage', 'director']
 
 
 class ActorAdmin(admin.ModelAdmin):
--- filmy/models.py
+++ filmy/models.py
@@ -4,17 +4,35 @@ class Movie(models.Model):
     name = models.CharField(max_length=30)
     year = models.PositiveSmallIntegerField(blank=True, null=True) # nepovinne, proto blank a null
     footage = models.PositiveSmallIntegerField(blank=True, null=True, help_text='in minutes') # delka filmu
-    main_picture = models.ImageField(blank=True, null=True)
+    main_picture = models.CharField(max_length=2000, blank=True, default='')
     description = models.TextField(blank=True, null=True)
+    # kdyz smazu rezisera tohodle filmu, tak se tenhle atribut nastavi na null
+    director = models.ForeignKey('Director', blank=True, null=True, on_delete=models.SET_NULL)
+    # on_delete se v techto pripadech nepouzije, kvuli ManyToMany vazbe, je jich tam nekolik
+    actors = models.ManyToManyField('Actor', blank=True, null=True)
+    genres = models.ManyToManyField('Genre', blank=True, null=True)
     
     def __str__(self):
         return self.name
+    
+    # vypise vsechny zanry s carkami
+    def genres_display(self):
+        # rozepsana metoda, kterou jsme pouzili:
+        
+        # genres = self.genres.all()
+        # out = ''
+        # for genre in genres:
+        #     out += f'[genre.name], '
+            
+        # return out
+        
+        return ', '.join(i.name for i in self.genres.all())
     
     
 class Actor(models.Model):
     name = models.CharField(max_length=30)
     birth_year = models.PositiveSmallIntegerField(blank=True, null=True)
-    main_picture = models.ImageField(blank=True, null=True)
+    main_picture = models.CharField(max_length=2000, blank=True, default='')
     description = models.TextField(blank=True, null=True)
     
     def __str__(self):
@@ -24,7 +42,7 @@ class Actor(models.Model):
 class Director(models.Model):
     name = models.CharField(max_length=30)
     birth_year = models.PositiveSmallIntegerField(blank=True, null=True)
-    main_picture = models.ImageField(blank=True, null=True)
+    main_picture = models.CharField(max_length=2000, blank=True, default='')
     description = models.TextField(blank=True, null=True)
     
     def __str__(self):
