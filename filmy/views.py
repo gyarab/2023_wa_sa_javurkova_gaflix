517d08669017040936ad79f12315c282ea1356c7
sabi <sabina.javurkova@student.gyarab.cz>
2024-05-31 11:00:56 +0200

    zobrazeni karet s filmy a info

--- filmy/views.py
+++ filmy/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from filmy.models import Movie
+
+def movies(request):
+    context = {
+        'movies': Movie.objects.all().order_by('name')
+    }
+    return render(request, 'filmy/movies.html', context)
+
+def movie(request, id):
+    context = {
+        'movie':  Movie.objects.get(id=id)
+    }
+    return render(request, 'filmy/movie.html', context)
