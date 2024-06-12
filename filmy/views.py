4914e99f5ed34051d7b5fb1d53e046daf83f2f0a
sabi <sabina.javurkova@student.gyarab.cz>
2024-06-12 22:02:35 +0200

    domaci ukol 12.6.

--- filmy/views.py
+++ filmy/views.py
@@ -1,15 +1,74 @@
 from django.shortcuts import render
 
-from filmy.models import Movie
+from filmy.models import Movie, Director, Actor
 
 def movies(request):
+    query = request.GET.get('query')
+    if query:
+        movies = Movie.objects.filter(name__icontains=query) | Movie.objects.filter(description__icontains=query)
+    else:
+        movies = Movie.objects.all().order_by('name')
+    
     context = {
-        'movies': Movie.objects.all().order_by('name')
+        'movies': movies,
+        'search': True
     }
     return render(request, 'filmy/movies.html', context)
+
 
 def movie(request, id):
     context = {
         'movie':  Movie.objects.get(id=id)
     }
     return render(request, 'filmy/movie.html', context)
+
+
+def actors(request):
+    query = request.GET.get('query')
+    if query:
+        actors = Actor.objects.filter(name__icontains=query)
+    else:
+        actors = Actor.objects.all().order_by('name')
+    
+    context = {
+        'actors': True,
+        'people': actors,
+        'search': True
+    }
+    return render(request, 'filmy/people.html', context)
+
+
+def actor(request, id):
+    actor = Actor.objects.get(id=id)
+    
+    context = {
+        'actor': True,
+        'person': actor,
+        'movies': Movie.objects.filter(actors=actor)
+    }
+    return render(request, 'filmy/person.html', context)
+
+
+def directors(request):
+    query = request.GET.get('query')
+    if query:
+        directors = Director.objects.filter(name__icontains=query)
+    else:
+        directors = Director.objects.all().order_by('name')
+    
+    context = {
+        'people': directors,
+        'search': True
+    }
+    return render(request, 'filmy/people.html', context)
+
+
+def director(request, id):
+    director = Director.objects.get(id=id)
+    
+    context = {
+        'person': director,
+        'movies': Movie.objects.filter(director=director)
+    }
+    return render(request, 'filmy/person.html', context)
+
--- gaflix/urls.py
+++ gaflix/urls.py
@@ -2,12 +2,16 @@ from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
 
-from filmy.views import movies, movie
+from filmy.views import movies, movie, director, directors, actor, actors
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name="filmy/index.html")),
     path('druhy/', TemplateView.as_view(template_name="filmy/druhy.html")),
     path('filmy/', movies, name='movies'),
-    path('film/<int:id>', movie, name='movie'),
+    path('film/<int:id>/', movie, name='movie'),
+    path('reziseri/', directors, name='directors'),
+    path('reziser/<int:id>/', director, name='director'),
+    path('herci/', actors, name='actors'),
+    path('herec/<int:id>/', actor, name='actor')
 ]
